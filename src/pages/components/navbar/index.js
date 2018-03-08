a72e6a729d29914a5dca283fda28ef9a45dd91d3
Cliff Pyles <cliff@cliffpyles.com>
2018-03-08 07:11:59 -0500

    Add Navbar component

---
+++ src/pages/components/navbar/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Documentation from '@siteComponents/Documentation'
+import Example from '@siteComponents/Example'
+import Navbar, {NavbarItem, Docs} from '@components/Navbar'
+
+export default () => {
+  return (
+    <Documentation docs={Docs}>
+      <Example heading="Navbar">
+        <Navbar>
+          <NavbarItem>Home</NavbarItem>
+          <NavbarItem>Services</NavbarItem>
+          <NavbarItem>Products</NavbarItem>
+          <NavbarItem>About</NavbarItem>
+          <NavbarItem>Contact</NavbarItem>
+        </Navbar>
+      </Example>
+
+      <Example heading="Vertical Navbar">
+        <Navbar className="is-vertical">
+          <NavbarItem>Home</NavbarItem>
+          <NavbarItem>Services</NavbarItem>
+          <NavbarItem>Products</NavbarItem>
+          <NavbarItem>About</NavbarItem>
+          <NavbarItem>Contact</NavbarItem>
+        </Navbar>
+      </Example>
+    </Documentation>
+  )
+}
