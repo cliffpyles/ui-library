05a133a2f3e90b9739abf59bbc1e80e9eb7009f6
Cliff Pyles <cliff@cliffpyles.com>
2018-03-08 09:03:37 -0500

    Update grid layout example content

--- src/pages/layouts/grid/index.js
+++ src/pages/layouts/grid/index.js
@@ -10,23 +10,18 @@ export default () => {
         <Grid>
           <div>
             <h3>Item 1 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, pariatur, perferendis. Eos repellendus, qui corrupti sint. Repellendus accusamus dicta, dignissimos aliquid doloremque eos deserunt in iusto, porro laboriosam, quibusdam beatae.</p>
           </div>
           <div>
             <h3>Item 2 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam consectetur, rem voluptas fugiat facere autem, saepe vero molestiae deserunt. Minus repellat tempora in, quisquam perspiciatis deleniti molestiae aut ratione numquam!</p>
           </div>
           <div>
             <h3>Item 3 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, ex quibusdam dicta corporis vitae iure ipsum praesentium ratione pariatur, fugit molestias minima, explicabo, facere labore accusantium quo est tempore officia.</p>
           </div>
           <div>
             <h3>Item 4 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore ipsam veritatis vitae assumenda, animi possimus eligendi iusto. Maxime cumque harum sequi a tempore distinctio, laboriosam, eveniet, qui iure recusandae perferendis.</p>
           </div>
           <div>
             <h3>Item 5 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid totam consequatur tenetur soluta recusandae, modi dignissimos ipsa numquam deleniti repudiandae et, voluptatibus assumenda odio, tempore iste necessitatibus nam magnam perspiciatis.</p>
           </div>
         </Grid>
       </Example>
@@ -35,23 +30,18 @@ export default () => {
         <Grid className="has-small-cells">
           <div>
             <h3>Item 1 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, pariatur, perferendis. Eos repellendus, qui corrupti sint. Repellendus accusamus dicta, dignissimos aliquid doloremque eos deserunt in iusto, porro laboriosam, quibusdam beatae.</p>
           </div>
           <div>
             <h3>Item 2 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam consectetur, rem voluptas fugiat facere autem, saepe vero molestiae deserunt. Minus repellat tempora in, quisquam perspiciatis deleniti molestiae aut ratione numquam!</p>
           </div>
           <div>
             <h3>Item 3 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, ex quibusdam dicta corporis vitae iure ipsum praesentium ratione pariatur, fugit molestias minima, explicabo, facere labore accusantium quo est tempore officia.</p>
           </div>
           <div>
             <h3>Item 4 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore ipsam veritatis vitae assumenda, animi possimus eligendi iusto. Maxime cumque harum sequi a tempore distinctio, laboriosam, eveniet, qui iure recusandae perferendis.</p>
           </div>
           <div>
             <h3>Item 5 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid totam consequatur tenetur soluta recusandae, modi dignissimos ipsa numquam deleniti repudiandae et, voluptatibus assumenda odio, tempore iste necessitatibus nam magnam perspiciatis.</p>
           </div>
         </Grid>
       </Example>
@@ -60,23 +50,18 @@ export default () => {
         <Grid className="has-extra-small-cells">
           <div>
             <h3>Item 1 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, pariatur, perferendis. Eos repellendus, qui corrupti sint. Repellendus accusamus dicta, dignissimos aliquid doloremque eos deserunt in iusto, porro laboriosam, quibusdam beatae.</p>
           </div>
           <div>
             <h3>Item 2 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam consectetur, rem voluptas fugiat facere autem, saepe vero molestiae deserunt. Minus repellat tempora in, quisquam perspiciatis deleniti molestiae aut ratione numquam!</p>
           </div>
           <div>
             <h3>Item 3 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, ex quibusdam dicta corporis vitae iure ipsum praesentium ratione pariatur, fugit molestias minima, explicabo, facere labore accusantium quo est tempore officia.</p>
           </div>
           <div>
             <h3>Item 4 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore ipsam veritatis vitae assumenda, animi possimus eligendi iusto. Maxime cumque harum sequi a tempore distinctio, laboriosam, eveniet, qui iure recusandae perferendis.</p>
           </div>
           <div>
             <h3>Item 5 Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid totam consequatur tenetur soluta recusandae, modi dignissimos ipsa numquam deleniti repudiandae et, voluptatibus assumenda odio, tempore iste necessitatibus nam magnam perspiciatis.</p>
           </div>
         </Grid>
       </Example>
