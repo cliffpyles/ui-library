2c813e5ee31c43ba852b7e58aea4700329d5877d
Cliff Pyles <cliff@cliffpyles.com>
2018-03-08 10:34:44 -0500

    Add Toolbar component

---
+++ src/pages/components/toolbar/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Documentation from '@siteComponents/Documentation'
+import Example from '@siteComponents/Example'
+import Toolbar, {ToolbarGroup, Docs} from '@components/Toolbar'
+
+export default () => {
+  return (
+    <Documentation docs={Docs}>
+      <Example heading="Toolbar">
+        <Toolbar>
+          <button>Button 1</button>
+          <button>Button 2</button>
+          <input type="text" value="search value"/>
+          <button>search</button>
+        </Toolbar>
+      </Example>
+
+      <Example heading="Toolbar with Toolbar Groups">
+        <Toolbar>
+          <ToolbarGroup>
+            <button>Button 1</button>
+            <button>Button 2</button>
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <input type="text" value="search value"/>
+            <button>search</button>
+          </ToolbarGroup>
+        </Toolbar>
+      </Example>
+
+      <Example heading="Toolbar with Spaced Toolbar Groups">
+        <Toolbar className="has-space-between">
+          <ToolbarGroup>
+            <button>Button 1</button>
+            <button>Button 2</button>
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <input type="text" value="search value"/>
+            <button>search</button>
+          </ToolbarGroup>
+        </Toolbar>
+      </Example>
+    </Documentation>
+  )
+}
