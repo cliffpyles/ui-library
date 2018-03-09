2cb904cf9c28de0426f3025553399d99dbe9d3dc
Cliff Pyles <cliff@cliffpyles.com>
2018-03-09 07:17:15 -0500

    Add card-view demo as a pattern

---
+++ src/pages/patterns/card-view/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Documentation from '@siteComponents/Documentation'
+import Example from '@siteComponents/Example'
+import CardView from '@patterns/CardView'
+
+export default () => {
+  return (
+    <Documentation>
+      <Example heading="Card View">
+        <CardView />
+      </Example>
+    </Documentation>
+  )
+}
