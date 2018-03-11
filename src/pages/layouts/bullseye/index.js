7008ae06f61672253e33b54ea0c23c5bc1441ddb
Cliff Pyles <cliff@cliffpyles.com>
2018-03-11 09:37:29 -0400

    Fix Bullseye positioning bug

--- src/pages/layouts/bullseye/index.js
+++ src/pages/layouts/bullseye/index.js
@@ -6,20 +6,14 @@ import Bullseye from '@layouts/Bullseye'
 export default () => {
   return (
     <Documentation className="is-bullseye-documentation">
-      <Example heading="Bullseye">
+      <Example heading="Bullseye" isViewport={true}>
         <Bullseye>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
         </Bullseye>
       </Example>
 
-      <Example heading="Bullseye with Body">
+      <Example heading="Bullseye with Body" isViewport={true}>
         <Bullseye hasBody={true}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        </Bullseye>
-      </Example>
-
-      <Example heading="Bullseye as Full Page">
-        <Bullseye className="is-full-page">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
         </Bullseye>
       </Example>
