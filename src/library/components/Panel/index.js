82bc28decd529287e58683cfe4b3caf3f07e8696
Cliff Pyles <cliff@cliffpyles.com>
2018-03-11 12:10:47 -0400

    Add collapse, groups, and titles to Panel

--- src/library/components/Panel/index.js
+++ src/library/components/Panel/index.js
@@ -2,12 +2,42 @@ import React from 'react'
 import docs from './docs.md'
 import './styles.scss'
 
-
-
-export default ({children, className = ''}) => {
+const PanelGroupHeader = ({children, className = '', title}) => {
   return (
-    <div className={`panel ${className}`}>
+    <div className={`panel__group__header ${className}`}>
+      {title && <h5 className="panel__group__header_title">{title}</h5>}
       {children}
+    </div>
+  )
+}
+
+export const PanelGroup = ({children, className = '', title}) => {
+  return (
+    <div className={`panel__group ${className}`}>
+      {title && <PanelGroupHeader title={title} />}
+      <div className="panel__group__body">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default ({children, className = '', title, isExpanded = true}) => {
+  const isExpandedClass = (isExpanded) ? '' : 'is-collapsed'
+
+  return (
+    <div className={`panel ${className} ${isExpandedClass}`}>
+      <div className="panel__header">
+        <button className="panel_toggle">
+          {isExpanded
+            ? <i class="fas fa-caret-right"></i>
+            : <i class="fas fa-caret-left"></i>}
+        </button>
+        {title && <h4 className="panel__header_title">{title}</h4>}
+      </div>
+      <div className="panel__body">
+        {children}
+      </div>
     </div>
   )
 }
--- src/pages/components/panel/index.js
+++ src/pages/components/panel/index.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import Documentation from '@systemComponents/Documentation'
 import Example from '@systemComponents/Example'
-import Panel from '@components/Panel'
+import Panel, {PanelGroup} from '@components/Panel'
 
 export default () => {
   return (
     <Documentation>
-      <Example heading="Panel on Left" isViewport={true}>
-        <Panel className="is-left">
+      <Example heading="Panel">
+        <Panel title="Title of Panel">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
         </Panel>
       </Example>
 
-      <Example heading="Panel on Right" isViewport={true}>
-        <Panel className="is-right">
+      <Example heading="Panel Collapsed">
+        <Panel title="Title of Panel" isExpanded={false}>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </Panel>
+      </Example>
+
+      <Example heading="Panel With Groups">
+        <Panel title="Title of Panel">
+          <PanelGroup title="First Panel Group">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </PanelGroup>
+          <PanelGroup title="Second Panel Group">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </PanelGroup>
+          <PanelGroup title="Third Panel Group">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </PanelGroup>
         </Panel>
       </Example>
     </Documentation>
