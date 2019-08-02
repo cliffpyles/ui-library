0ddf664760b8fd4025d63c818bc9f586c3eaefa9
Cliff Pyles <cliff@cliffpyles.com>
2019-08-02 18:32:56 -0400

    Add Clickable blueprint

--- .blueprints/clickable/files/src/components/__blueprintInstance__/__blueprintInstance__.js
+++ .blueprints/clickable/files/src/components/__blueprintInstance__/__blueprintInstance__.js
@@ -2,14 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './{{blueprintInstance}}.scss';
 
-const {{blueprintInstance}} = ({ children, className = '', onClick }) => {
-  return <{{blueprintInstance}} className={`{{blueprintInstance}} ${className}`} onClick={onClick}>{children}</{{blueprintInstance}}>;
+const {{blueprintInstance}} = ({ children, className = '', onClick, style = {} }) => {
+  return <{{blueprintInstance}} className={`{{blueprintInstance}} ${className}`} onClick={onClick} style={style}>{children}</{{blueprintInstance}}>;
 };
 
 {{blueprintInstance}}.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
   onClick: PropTypes.func.isRequired,
+  style: PropTypes.object
 };
 
 export default {{blueprintInstance}};
