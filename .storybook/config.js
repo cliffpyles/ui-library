70b8c1e3767373e47f8df07575a9484bbe423da1
Cliff Pyles <cliff@cliffpyles.com>
2019-08-04 10:45:09 -0400

    Add decorator for applying hierarchy classes to all stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,20 @@
-import { configure } from "@storybook/react";
+import React from "react";
+import { configure, addDecorator } from "@storybook/react";
 import "./styles.scss";
 import "../src/index.css";
+
+addDecorator((storyFn, props) => {
+  const results = Array.from(props.kind.matchAll(/(\w*)\|(\w*)\/?(\w*)/g));
+  let [result, categoryName, sectionName, storyName] = results[0];
+
+  return (
+    <div
+      className={`${categoryName || "Unspecified"}Category ${sectionName ||
+        "Unspecified"}Section ${storyName || sectionName}Story`}
+    >
+      {storyFn()}
+    </div>
+  );
+});
 
 configure(require.context("../src", true, /\.stories\.js$/), module);
--- src/apps/Chat/Chat.js
+++ src/apps/Chat/Chat.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Classic, { ClassicPrimary, ClassicSecondary } from '../../components/Classic';
-import './Chat.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import Workspace, {
+  WorkspaceContext,
+  WorkspaceDetails,
+  WorkspaceFooter,
+  WorkspaceHeader,
+  WorkspaceMain,
+  WorkspaceToolbar
+} from "../../components/Workspace";
+import "./Chat.scss";
 
-const Chat = ({ children, className = '', style = {} }) => {
-  return <div className={`Chat ${className}`} style={style}>
-    <Classic>
-      <ClassicSecondary>
-        sidebar
-      </ClassicSecondary>
-      <ClassicPrimary>
-        main
-      </ClassicPrimary>
-    </Classic>
-  </div>;
+const Chat = ({ children, className = "", style = {} }) => {
+  return (
+    <div className={`Chat ${className}`} style={style}>
+      <Workspace>
+        <WorkspaceHeader>placeholder</WorkspaceHeader>
+        <WorkspaceToolbar>placeholder</WorkspaceToolbar>
+        <WorkspaceContext>placeholder</WorkspaceContext>
+        <WorkspaceMain>placeholder</WorkspaceMain>
+        <WorkspaceFooter>placeholder</WorkspaceFooter>
+        <WorkspaceDetails>placeholder</WorkspaceDetails>
+      </Workspace>
+    </div>
+  );
 };
 
 Chat.propTypes = {
