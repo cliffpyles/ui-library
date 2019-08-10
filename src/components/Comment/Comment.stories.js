91203993da3676b63ba8bc827e67eab92cfb00d1
Cliff Pyles <cliff@cliffpyles.com>
2019-08-10 06:31:01 -0400

    Update comment story props

--- src/components/Comment/Comment.stories.js
+++ src/components/Comment/Comment.stories.js
@@ -1,11 +1,16 @@
 import React from "react";
+import faker from "faker";
 import Comment from "./Comment";
 
 export default {
   title: "Components|Content/Comment"
 };
 
+const user = faker.helpers.contextualCard();
 const props = {
-  author: "Jane Doe"
+  author: user.username,
+  avatar: <img src={user.avatar} />,
+  createdAt: `Sun, 04 Aug 2019 15:25:04 GMT`,
+  updatedAt: `Sun, 04 Aug 2019 15:25:04 GMT`
 };
 export const Default = () => <Comment {...props}>Hello, world!</Comment>;
