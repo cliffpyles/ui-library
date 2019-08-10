fee77e290bdb5d886c583f7a0fd836f42e1cfbe9
Cliff Pyles <cliff@cliffpyles.com>
2019-08-10 14:44:08 -0400

    Add stories for Card

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -1,8 +1,44 @@
 import React from "react";
-import Card from "./Card";
+import Card, { CardHeader, CardMain, CardFooter } from "./Card";
 
 export default {
-  title: "Components|Content/Card"
+  title: "Components|Content/Card",
+  component: Card
 };
 
-export const Default = () => <Card>child content</Card>;
+export const CardDefault = () => <Card>child content</Card>;
+
+CardDefault.story = {
+  name: "default"
+};
+
+export const CardWithHeader = () => (
+  <Card>
+    <CardHeader>
+      <span>header content</span>
+    </CardHeader>
+    <span>child content</span>
+  </Card>
+);
+
+CardWithHeader.story = {
+  name: "with header"
+};
+
+export const CardWithAll = () => (
+  <Card>
+    <CardHeader>
+      <span>header content</span>
+    </CardHeader>
+    <CardMain>
+      <span>main content</span>
+    </CardMain>
+    <CardFooter>
+      <span>footer content</span>
+    </CardFooter>
+  </Card>
+);
+
+CardWithAll.story = {
+  name: "with all extras"
+};
