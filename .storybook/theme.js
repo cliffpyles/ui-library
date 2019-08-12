a7773ca1b8bad2308f193ee0b97f3f0d6f7efc30
Cliff Pyles <cliff@cliffpyles.com>
2019-08-12 06:37:41 -0400

    Enable layout styles

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,6 +1,26 @@
 import { create } from "@storybook/theming";
 import glassesOpaque from "./img/glasses-opaque.svg";
 
+const black = "#232425";
+const white = "#ffffff";
+const spaceCadet = "#1d3557";
+const queenBlue = "#457b9d";
+const lightBlue = "#a8dadc";
+const honeyDew = "#f1faee";
+const desire = "#e63946";
+
+const appBg = "#fafafa";
+const appBorderColor = lightBlue;
+const appContentBg = white;
+const barBg = spaceCadet;
+const barSelectedColor = lightBlue;
+const barTextColor = white;
+const textColor = spaceCadet;
+
+const inputBg = white;
+
+const colorSecondary = lightBlue;
+
 export default create({
   base: "light",
   brandTitle: "UI Library",
